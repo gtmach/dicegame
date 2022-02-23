5e564c175e3ce3fb6819225123c217bda552840f
gtmach <97925593+gtmach@users.noreply.github.com>
2022-02-23 00:27:09 -0300

    Update index.js
    Add event listener to button

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-var randomNumber1 = (Math.floor(Math.random() * 6) + 1);
-var randomNumber2 = (Math.floor(Math.random() * 6) + 1);
 
-var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
+var randomNumber1 = 0
+var randomNumber2 = 0
+
+var randomDiceImage1 = 0
+var randomDiceImage2 = 0
+
+function rollDice(){
+randomNumber1 = (Math.floor(Math.random() * 6) + 1);
+randomNumber2 = (Math.floor(Math.random() * 6) + 1);
+
+randomDiceImage1 = "images/dice" + randomNumber1 + ".png"
+randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
 
 document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
-if (randomNumber1 > randomNumber2) {
+  if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Jogador 1 ganhou!";
 } else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Jogador 2 ganhou! 🚩";
 } else {
   document.querySelector("h1").innerHTML = "Empate!";
 }
+
+}
+
+document.querySelector("button").addEventListener("click", rollDice);
